1494574d1e7f287ef49d553bd8e87576a750e9e9
grazielleagcaoili <85484955+grazielleagcaoili@users.noreply.github.com>
2021-09-24 21:04:53 +0800

    src

---
+++ src/functions/datagrid.js
@@ -0,0 +1,97 @@
+import { onDownloadHistory, onUpdateDatagrid } from '../services/studyAPI';
+import { notification } from 'antd';
+
+export const notif = (type, message) => {
+    notification[type]({
+      message: 'Notification Title',
+      description:
+        message,
+    });
+  };
+
+
+
+
+export async function downloadCSV(data, ID, title, user){
+    let csv = ''
+
+    let keys = Object.keys(data[0])
+    keys.forEach((key) => {
+      csv += key + ","
+    })
+    csv += "\n"
+
+    data.forEach((datarow) => {
+      keys.forEach((key)=>{
+        csv += datarow[key] + ","
+      })
+      csv += "\n"
+    });
+      const element = document.createElement('a')
+      const file = new Blob([csv], {type: 'data:text/csv;charset=utf-8'})
+      element.href = URL.createObjectURL(file)
+      element.download = `${title}.csv`
+      document.body.appendChild(element)
+      element.click()
+      let id ={tableID: ID}
+      let resultDownload = await onDownloadHistory({user: user , id})
+      notif('success', resultDownload.data.message)
+}
+
+
+export async function downloadCSVonCreate(data, title){
+  
+    try {
+      let csv = ''
+    let keys = Object.keys(data[0])
+    keys.forEach((key) => {
+      csv += key + ","
+    })
+    csv += "\n"
+
+    data.forEach((datarow) => {
+      keys.forEach((key)=>{
+        csv += datarow[key] + ","
+      })
+      csv += "\n"
+    });
+      const element = document.createElement('a')
+      const file = new Blob([csv], {type: 'data:text/csv;charset=utf-8'})
+      element.href = URL.createObjectURL(file)
+      element.download = `${title}.csv`
+      document.body.appendChild(element)
+      element.click()
+      notif('success', "Download sucessful!")
+    } catch (error) {
+      notif('error', 'Download failed!')
+    }
+      
+  }
+
+  export async function downloadCSVonGrid(data){
+    let toDownload = data[0].data
+    let csv = ''
+    let keys = Object.keys(data[0].data[0])
+    keys.forEach((key) => {
+      csv += key + ","
+    })
+    csv += "\n"
+
+    toDownload.forEach((datarow) => {
+      keys.forEach((key)=>{
+        csv += datarow[key] + ","
+      })
+      csv += "\n"
+    });
+      const element = document.createElement('a')
+      const file = new Blob([csv], {type: 'data:text/csv;charset=utf-8'})
+      element.href = URL.createObjectURL(file)
+      element.download = `${data[0].title}.csv`
+      document.body.appendChild(element)
+      element.click()
+  }
+
+  export async function updateDB(data){
+    let result = await onUpdateDatagrid(data)
+    notif('success', result.data.message)
+  }
---
+++ src/services/socket.js
@@ -0,0 +1,25 @@
+import {io} from 'socket.io-client'
+
+export const socket = io("http://localhost:3002")
+
+export const join = (id, user, join) =>{
+    socket.emit('join-table', {room: id, user: user, join: join})
+}
+
+export const changeColumns = (type, title, id) => {
+    socket.emit("send-changes-columns",
+      {
+        type: type, 
+        title: title, 
+        room: id
+      })
+}
+
+export const columnDelete = (data, id) =>{
+    socket.emit("send-changes-columns-delete", {data: data, room: id})
+}
+
+
+export const emitDatagridChange = (data, id) =>{
+    socket.emit('send-changes', {data: data, room: id})
+}
