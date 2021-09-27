0a3300d6055ad40eac5dfd2a4c05c9134b40adb6
grazielleagcaoili <85484955+grazielleagcaoili@users.noreply.github.com>
2021-09-27 14:24:48 +0800

    build

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client'
 
-export const socket = io("http://localhost:3002")
+export const socket = io("http://nberic.org:3002")
 
 export const join = (id, user, join) =>{
     socket.emit('join-table', {room: id, user: user, join: join})
